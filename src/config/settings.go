5d2da87a3b148853e0727e7063a73984ba1f25fa
@milesibastos <antonio@milesibastos.com>
2025-12-19 15:51:15 -0300

    chore: bump version to v7.11.0+2
    - AppVersion: v7.11.0+2
    - Chart version: 7.11.2
    - Docker tag: v7.11.0-2

--- src/config/settings.go
+++ src/config/settings.go
@@ -5,7 +5,7 @@ import (
 )
 
 var (
-	AppVersion             = "v7.11.0+1"
+	AppVersion             = "v7.11.0+2"
 	AppPort                = "3000"
 	AppDebug               = false
 	AppOs                  = "AldinoKemal"
